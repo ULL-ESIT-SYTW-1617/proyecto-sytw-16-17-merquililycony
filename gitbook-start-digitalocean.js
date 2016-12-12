6388dc928ce32ee821f3292921c3de619a9103eb
Merquis Jesús Cruz Veloz <alu0100536690@ull.edu.es>
2016-12-12 06:23:09 +0000

    Quitando código sobrante

--- gitbook-start-digitalocean.js
+++ gitbook-start-digitalocean.js
@@ -12,24 +12,42 @@ var Curl = require('node-libcurl').Curl;
 var curl = new Curl();
 
 
-var usuario = readlineSync.question('Introduzca el USUARIO de github: ');
-var password = readlineSync.question('Introduzca su contraseña de github: ', { hideEchoBack: true });
+function checkDirectorySync(directory) {
+  try {
+    fs.statSync(directory);
+  } catch(e) {
+    fs.mkdirSync(directory);
 
-// var args = " -u "+usuario+":"+password+" -d ";
-// var args1 = '\'{"scopes": ["repo", "user"], "note":"'+usuario+'"}\'';
-// var args2 = " https://api.github.com/authorizations >> "+usuario+".json";
-// var crear_token = args + args1 + args2;
-// exec('curl ' + crear_token);
+    var usuario = readlineSync.question('Introduzca el USUARIO de github: ');
+    var password = readlineSync.question('Introduzca su contraseña de github: ', { hideEchoBack: true });
+
+    fe.mkdirs('.gitbook-start', function (err) {
+      if (err) return console.error(err)
+      console.log("success!")
+    })
+
+    var args = " -u "+usuario+":"+password+" -d ";
+    var args1 = '\'{"scopes": ["repo", "user"], "note":"'+usuario+'"}\'';
+    var args2 = " https://api.github.com/authorizations >> .gitbook-start/config.json";
+    var crear_token = args + args1 + args2;
+    exec('curl ' + crear_token);
+  }
+}
+
+checkDirectorySync("./.gitbook-start");
 
 // //COGER TOKEN
-// var json = JSON.parse(fs.readFileSync(usuario+'.json','utf8'))
- var json = JSON.parse(fs.readFileSync('./package.json','utf8'));
- //json -I -f package.json -e 'this.repository.url="bebe"'
-dir = json.Directorio.nombre_dir;
-exec('json -I -f package.json -e \'this.repository.url=\"'+"https://github.com/"+usuario+"/"+dir+".git"+'\"\'');//URL REMOTA
+var json_token = JSON.parse(fs.readFileSync('.gitbook-start/config.json','utf8'))
 
-//console.log("Token usuario: "+token);
+var token = json_token.token;
+var usuario_tok = json_token.app.name;
+console.log("Usuario: "+usuario_tok);
+console.log("Token: "+token);
 
+var json = JSON.parse(fs.readFileSync('./package.json','utf8'));
+var dir = json.nombre_dir; //Utilizar el que está comentado arriba cuando instalemos los paquetes.
+
+exec('json -I -f package.json -e \'this.repository.url=\"'+"https://github.com/"+usuario_tok+"/"+dir+".git"+'\"\'');//URL REMOTA
 
 //CREAR REPOSITORIO REMOTO EN GITHUB CON EL TOKEN
 var pwd = function(pwd, callback){
@@ -41,20 +59,10 @@ var pwd = function(pwd, callback){
 };
 
 var getPwd = function(repo_name){
-  exec('curl -u '+"\""+usuario+"\":\""+password+"\" https://api.github.com/user/repos -d "+'\'{"name":"'+repo_name+'"}\'');
-   // exec('git remote add origin git@github.com:'+usuario+'/'+repo_name+'.git; git push -u origin master');
-    //   exec('git init; git add README.md; git commit -m "first commit; git remote add origin git@github.com:'+usuario+'/'+repo_name+'.git; git push -u origin master');
+  exec('curl -u '+"\""+usuario_tok+"\":\""+token+"\" https://api.github.com/user/repos -d "+'\'{"name":"'+repo_name+'"}\'');
+  // exec('git remote add origin git@github.com:'+usuario+'/'+repo_name+'.git; git push -u origin master');
+  //   exec('git init; git add README.md; git commit -m "first commit; git remote add origin git@github.com:'+usuario+'/'+repo_name+'.git; git push -u origin master');
 
 }
 
 pwd("pwd", getPwd);
-
-
-// funcion deploy
-// var initialize = function(){
-//   exec('git init; git add README.md; git commit -m "first commit; git remote add origin git@github.com:'+usuario+'/'+repo_name+'.git; git push -u origin master');
-// }
-// var deploy = function(){
-
-//   exec('git add .; git commit -m "Deploy to Heroku"; git push heroku ');
-// }
