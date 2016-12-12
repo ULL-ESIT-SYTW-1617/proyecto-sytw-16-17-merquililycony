ad1e7a7670b4a220e130c1f50b82a2d92ee85caa
Merquis Jesús Cruz Veloz <alu0100536690@ull.edu.es>
2016-12-12 06:08:51 +0000

    Configurando servidor

--- template/app.js
+++ template/app.js
@@ -1,20 +1,27 @@
 // importar
     var express = require('express');
+
+    // instanciar
     var app = express();
     var path = require('path');
+
+
+   // ruteo
+
+   app.use(express.static(path.join(__dirname,'gh-pages')));
+
+
+   app.get('/', function(request, response) {
+     response.send('index');
+   });
 
 
     // escuchar
    // app.listen(80);
    app.set('port', process.env.PORT || 80);
 
-    deploy_digitalocean.push_digitalocean();
 
-
-//digitalocean git:remote -a digitalocean-project-name
 app.listen(app.get('port'), function() {
   console.log('Servidor escuchando en el puerto:'+app.get('port'));
 });
 
-
-module.exports = app;
