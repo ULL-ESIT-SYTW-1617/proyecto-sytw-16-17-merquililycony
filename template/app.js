e2a767b7b381a8c935d40caed520dd9fa0fe8cf2
Merquis Jesús Cruz Veloz <alu0100536690@ull.edu.es>
2016-12-17 16:04:02 +0000

    Separando http y https

--- template/app.js
+++ template/app.js
@@ -6,19 +6,33 @@ var express = require('express');
 var app = express();
 
 
-var privateKey  = fs.readFileSync('./tls/key.pem', 'utf8');
-var certificate = fs.readFileSync('./tls/cert.pem', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
+function checkDirectorySync(directory) {
+  try {
+    fs.statSync(directory); //Busca directorio tls
+  } catch(e) {
+
+    app.use(express.static(path.join(__dirname,'gh-pages')));
+    app.get('/', function(request, response) {
+      response.send('index');
+    });
+
+    var httpServer = http.createServer(app);
+    httpServer.listen(8080);
+  }
+
+  var privateKey  = fs.readFileSync('./tls/key.pem', 'utf8');
+  var certificate = fs.readFileSync('./tls/cert.pem', 'utf8');
+  var credentials = {key: privateKey, cert: certificate};
+
+  app.use(express.static(path.join(__dirname,'gh-pages')));
+  app.get('/', function(request, response) {
+    response.send('index');
+  });
+  var httpServer = http.createServer(app);
+  var httpsServer = https.createServer(credentials, app);
+  httpServer.listen(8080);
+  httpsServer.listen(443);
 
 
-app.use(express.static(path.join(__dirname,'gh-pages')));
-app.get('/', function(request, response) {
- response.send('index');
-});
-
-
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-httpServer.listen(8080);
-httpsServer.listen(443);
+}
+checkDirectorySync("./tls");
